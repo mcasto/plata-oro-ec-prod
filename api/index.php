cf759d51b2eaad7049891dd17275e1360f0f6561
Mike Casto <castoware@gmail.com>
2022-08-12 20:13:16 -0500

    Update

--- api/index.php
+++ api/index.php
@@ -18,7 +18,20 @@ $request = new Request;
 require_once(__DIR__ . '/methods/index.php');
 
 $router->addRoutes([
-  ['get', '/api/get-contents/[:path]?', 'getContents']
+  ['get', '/api/get-contents/[:path]?', 'getContents'],
+  ['get', '/api/get-contact-id/[:subject]', function ($db, $request) {
+    $idFile = __DIR__ . '/id-file.txt';
+    if (!file_exists($idFile)) file_put_contents($idFile, '0');
+    $prevID = trim(file_get_contents(__DIR__ . '/id-file.txt'));
+    $id = $prevID + 1;
+    file_put_contents(__DIR__ . '/id-file.txt', $id);
+    $s = explode(" ", urldecode($request->params->subject));
+    $sid = array_map(function ($word) {
+      return substr($word, 0, 1);
+    }, $s);
+
+    print "POE-" . strtoupper(implode("", $sid)) . "-" . sprintf("%06s", $id);
+  }]
 ]);
 
 $match = $router->match();
